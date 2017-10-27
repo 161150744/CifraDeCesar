cd828972ef5bfe7b6510409fb7981dee1eaf7f83
Felipe Melchior <fehmel@gmail.com>
2017-10-27 19:57:03 -0200

    Argumentos corrigidos

--- cifradecesar.py
+++ cifradecesar.py
@@ -3,24 +3,25 @@
 import argparse
 
 def getArguments():
-	parse = argparse.ArgumentParser(
+	parser = argparse.ArgumentParser(
 		description="Um programa que Criptografa e Descriptografa Cifra de Cesar")
 	
-	parse.add_argument('--t', action = 'store', dest = 'frase', required = True,
+	parser.add_argument('-t', action = 'store', dest = 'frase', required = True,
 					  default = 'Uma frase eh necessaria para a criptografia/descriptografia',
 					  help = 'Frase a ser criptografada/descriptografada')
 
-	parse.add_argument('--c', action = 'store', dest = 'opcao'  required = False, 
+	parser.add_argument('-c', action = 'store', dest = 'opcao', required = False, 
 					  default = 'Alo',
 					  help = 'Criptografa uma frase')
 
-	parse.add_argument('--d', action = 'store', dest = 'opcao'  required = False, 
+	parser.add_argument('-d', action = 'store', dest = 'opcao',  required = False, 
 					  default = 'Alo',
 					  help = 'Descriptografa uma frase')
 
-	arguments = parser.parse.args()
+	arguments = parser.parse_args()
 
 	for i in range(0, int(arguments.n)):
 		print arguments.t
 
 if __name__ == '__main__':
+	getArguments();
