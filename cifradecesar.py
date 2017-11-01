26b21ce2afc0090e9ddef993974f0d1cbb716741
Felipe Melchior <fehmel@gmail.com>
2017-11-01 14:44:58 -0200

    Adicao escopo das funcoes se relacionando com argumentos

--- cifradecesar.py
+++ cifradecesar.py
@@ -10,18 +10,57 @@ def getArguments():
 					  default = 'Uma frase eh necessaria para a criptografia/descriptografia',
 					  help = 'Frase a ser criptografada/descriptografada')
 
-	parser.add_argument('-c', action = 'store', dest = 'opcao', required = False, 
-					  default = 'Alo',
+	parser.add_argument('-c', action = 'store_true', dest = 'opcao', required = False, 
+					  default = None,
 					  help = 'Criptografa uma frase')
 
-	parser.add_argument('-d', action = 'store', dest = 'opcao',  required = False, 
-					  default = 'Alo',
+	parser.add_argument('-d', action = 'store_false', dest = 'opcao',  required = False, 
+					  default = None,
 					  help = 'Descriptografa uma frase')
+
+	parser.add_argument('-k', action = 'store', dest = 'chave', required = False,
+					   default = None,
+					   help = 'Chave usada para criptografar/descriptografar a frase')
 
 	arguments = parser.parse_args()
 
-	for i in range(0, int(arguments.n)):
-		print arguments.t
+	return arguments
+
+
+def criptografa(frase, chave):
+	print(frase,chave)
+
+def descriptografaComChave(frase, chave):
+	print("DescComChave")
+def descriptografaSemChave(frase):
+	print("DescSemChave")
+
+'''
+def desenhalogo():
+	print("/  ]|    ||     ||    \  /    |    |   \    /  _]       /  ]  /  _]/ ___/ /    ||    \ \n"
+  		 "/  /  |  | |   __||  D  )|  o  |    |    \  /  [_       /  /  /  [_(   \_ |  o  ||  D  ) \n"
+ 		"/  /   |  | |  |_  |    / |     |    |  D  ||    _]     /  /  |    _]\__  ||     ||    / \n" 
+	   "/   \_  |  | |   _] |    \ |  _  |    |     ||   [_     /   \_ |   [_ /  \ ||  _  ||    \ \n"	
+	   "\     | |  | |  |   |  .  \|  |  |    |     ||     |    \     ||     |\    ||  |  ||  .  \ \n"
+ 		"\____||____||__|   |__|\_||__|__|    |_____||_____|     \____||_____| \___||__|__||__|\_| \n")
+'''
 
 if __name__ == '__main__':
-	getArguments();
+	arguments = getArguments();
+
+	#desenhalogo()
+	
+	if(arguments.opcao == True):
+		if(arguments.chave != None):
+			criptografa(arguments.frase, arguments.chave)
+		else:
+			print("Para Criptografar uma frase, o uso de uma chave eh obrigatoria")
+			exit(1)
+
+	if(arguments.opcao == False):
+		if(arguments.chave != None):
+			descriptografaComChave(arguments.frase, arguments.chave)
+		else:
+			descriptografaSemChave(arguments.frase)
+
+
