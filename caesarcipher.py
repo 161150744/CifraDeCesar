891b3bc617eebae7d36e3e9bcd264996d5baaedb
Felipe <fehmel@gmail.com>
2018-03-21 23:01:09 -0300

    Adicionado menus, faltando apenas a logica das letras

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,40 +1,76 @@
+import sys
 import os
 import platform
 
-try:
-	import colorama
-	from colorama import Fore, Back, Style
-except ImportError:
-	print("ERRO! - Necessario biblioteca COLORAMA \n Instale com - easy_install colorama")
+from banner_script import *
 
-colorama.init()
-
-def desenhaBanner():
-	banner = """
- ██████╗██╗███████╗██████╗  █████╗     ██████╗ ███████╗     ██████╗███████╗███████╗ █████╗ ██████╗ 
-██╔════╝██║██╔════╝██╔══██╗██╔══██╗    ██╔══██╗██╔════╝    ██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗
-██║     ██║█████╗  ██████╔╝███████║    ██║  ██║█████╗      ██║     █████╗  ███████╗███████║██████╔╝
-██║     ██║██╔══╝  ██╔══██╗██╔══██║    ██║  ██║██╔══╝      ██║     ██╔══╝  ╚════██║██╔══██║██╔══██╗
-╚██████╗██║██║     ██║  ██║██║  ██║    ██████╔╝███████╗    ╚██████╗███████╗███████║██║  ██║██║  ██║
- ╚═════╝╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝    ╚═════╝ ╚══════╝     ╚═════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
- 			Criado por Felipe Homrich Melchior - UNIPAMPA
- 	        		www.homdreen.github.io
- 			caesarcipher.py --help para mais funções
-
- 			- [1] - CRIPTOGRAFAR
- 			- [2] - DESCRIPTOGRAFAR SEM CHAVE
- 			- [3] - DESCRIPTOGRAFAR COM CHAVE"""
-
-	print(Fore.WHITE+banner)
-
-def main():
+def limpaTela():
 	so = platform.system()
 	if(so == "Windows"):
 		os.system("cls")
 	else:
 		os.system("clear")
 
+def criptografa():
+	limpaTela()
+	desenhaCript()
+
+	try:
+		texto = input("Digite o texto para ser criptografado => ").upper()
+	except ValueError:
+		print("ERRO! - A entrada deve ser um texto/frase")
+
+
+	try:
+		chave = int(input("Digite a chave para ser na criptografado => "))
+	except ValueError:
+		print("ERRO! - A chave deve ser um número")
+
+	if((chave < 0) or (chave > 26)):
+		print("ERRO! - A chave deve ser um numero valido em relação ao alfabeto")
+		sys.exit()
+
+	lista = [texto]
+
+	for i in lista:
+		print(i)
+
+
+def descriptografaSemChave():
+	limpaTela()
+	desenhaDecript()
+
+
+def descriptografaComChave():
+	limpaTela()
+	desenhaDecriptKey()
+
+def menu(opcao):
+	if(opcao == 1):
+		print("Criptografa")
+		criptografa()
+	elif(opcao == 2):
+		print("DSC")
+		descriptografaSemChave()
+	elif(opcao == 3):
+		print("DCC")
+		#descriptografaComChave()
+	else:
+		print("Até a próxima!")
+		sys.exit()
+
+
+def main():
+	limpaTela()
+
 	desenhaBanner()
+
+	try:
+		opcao = int(input("Digite a opcao desejada => "))
+	except ValueError:
+		print("ERRO! - O valor deve ser um numero inteiro!")
+
+	menu(opcao)
 
 if __name__ == '__main__':
 	main()
